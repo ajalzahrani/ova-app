4b9283fc8863a7a033617ae89068d49ac1b09cff
Abdulrhaman Alzahrani <alzahrani.abdulrahman3@gmail.com>
2025-05-26 12:25:55 +0300

    add patient demo card

---
+++ src/app/occurrences/components/patient-demo-card.tsx
@@ -0,0 +1,44 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export interface PatientDemoCardProps {
+  FirstName: string;
+  Lastname: string;
+  MRN: string;
+  MobileNo: string;
+}
+
+export function PatientDemoCard({
+  patientDetails,
+}: {
+  patientDetails: PatientDemoCardProps;
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Patient Details</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <div className="flex flex-row gap-2">
+          <div className="flex flex-col gap-2">
+            <span className="text-sm text-muted-foreground">Name</span>
+            <span className="font-normal text-sm text-foreground">
+              {patientDetails.FirstName} {patientDetails.Lastname}
+            </span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="text-sm text-muted-foreground">MRN</span>
+            <span className="font-normal text-sm text-foreground">
+              {patientDetails.MRN}
+            </span>
+          </div>
+          <div className="flex flex-col gap-2">
+            <span className="text-sm text-muted-foreground">Mobile No</span>
+            <span className="font-normal text-sm text-foreground">
+              {patientDetails.MobileNo}
+            </span>
+          </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
